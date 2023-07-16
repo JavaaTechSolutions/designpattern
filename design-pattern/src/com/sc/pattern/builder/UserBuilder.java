6baf8cebc8081a6db711b18b0d02c306beddeb93
unknown <chakrabortyshiladittya2@gmail.com>
2023-07-16 23:52:09 +0530

    Added builder pattern code

---
+++ design-pattern/src/com/sc/pattern/builder/UserBuilder.java
@@ -0,0 +1,21 @@
+package com.sc.pattern.builder;
+
+public class UserBuilder {
+	public static void main(String[] args) {
+		User user = new User.UserBuilder("John", "Allen")
+				.age(30)
+				.address("US")
+				.phone("1234567890")
+				.build();
+		
+		System.out.println("User Object::"+user);
+		
+		user = new User.UserBuilder("John", "Allen")
+				.age(30)
+//				.address("US")
+				.phone("1234567890")
+				.build();
+		
+		System.out.println("User Object::"+user);
+	}
+}
